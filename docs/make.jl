89edca068f0e6d555dba8610d1573b5ecfff3163
Riccardo Boero <ribo@nilu.no>
2026-01-14 09:50:03 -0500

    fix logos

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,8 @@ makedocs(
     sitename = "JCGE",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", "false") == "true",
-        assets = ["assets/jcge_logo_light.png", "assets/jcge_logo_dark.png"],
+        logo = "assets/jcge_logo_light.png",
+        logo_dark = "assets/jcge_logo_dark.png",
     ),
     pages = [
         "Home" => "index.md",
